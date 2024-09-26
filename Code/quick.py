fe23fa60fb21d0b7d581206f8db2e904592bb1f8
Casper van Wetten <c.o.vanwetten@gmail.com>
2024-09-27 00:38:50 +0200

    Updated requirements and removed some old files

--- Code/quick.py
+++ Code/quick.py
@@ -1,19 +1,50 @@
-from helpers import get_input
-# Pretend that these are imported from the tokenizer
-text = get_input("wouter")
-chars = sorted(list(set(text)))
-stringtoint = { ch:i for i,ch in enumerate(chars) }
-encode = lambda s: [stringtoint[c] for c in s] # backup encoding algo
-inttostring = { i:ch for i,ch in enumerate(chars) }
-decode = lambda intL: ''.join([inttostring[i] for i in intL]) # backup decoding algo
+# from helpers import get_input
+# # Pretend that these are imported from the tokenizer
+# text = get_input("wouter")
+# chars = sorted(list(set(text)))
+# stringtoint = { ch:i for i,ch in enumerate(chars) }
+# encode = lambda s: [stringtoint[c] for c in s] # backup encoding algo
+# inttostring = { i:ch for i,ch in enumerate(chars) }
+# decode = lambda intL: ''.join([inttostring[i] for i in intL]) # backup decoding algo
 
-from GPT import Transformer
+# from GPT import Transformer
 
-tranny = Transformer()
-tranny.optimize()
+# tranny = Transformer()
+# tranny.optimize()
 
-context = "Wat is uw naam?"
-encoded = encode(context)
-print(context)
-print()
-print(decode(tranny.generate(context=encoded)[0].tolist()))
+# context = "Wat is uw naam?"
+# encoded = encode(context)
+# print(context)
+# print()
+# print(decode(tranny.generate(context=encoded)[0].tolist()))
+
+# from GPTInterface import Transformer
+# import torch
+
+# device = torch.device('cpu')
+# transformer = Transformer()
+# transformer.model.load_state_dict(torch.load("Code/models/default.pt", map_location=device, weights_only=True))
+# print("HUH")
+
+# transformer.model.generate("Misdrijven", 64)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
