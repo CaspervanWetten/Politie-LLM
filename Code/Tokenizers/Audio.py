1c9ede908710eeb00666e958a3bce6fe5363a380
Casper van Wetten <c.o.vanwetten@gmail.com>
2024-09-30 00:19:14 +0200

    Setup the basics for audio tokenizer

--- Code/Tokenizers/Audio.py
+++ Code/Tokenizers/Audio.py
@@ -4,14 +4,34 @@
 # Sample rate (resolution of audio)
 import torch
 import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
 import torchaudio.compliance.kaldi as ta_kaldi
 
+from IPython.display import Audio
+from matplotlib.patches import Rectangle
 from typing import Optional
 from Tokenizers.Generic import GenericTokenizer
+
+
 
 class AudioTokenizer(GenericTokenizer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # wouter.txt is zo'n 23k karakters, met letter-level encoding zoals dat nu was, is dat 23k tokens. 
+        # op 23k tokens / (60 tokens x 1 seconden) =  ~400 seconden = 15 seconden
+        # Ruige datagok: ~ een half uur van elke soort motor (V-twin, inline twin, single cylinder), gelabeld
+
+
+
+
+        self.num_dictionary: int = 8    # SOMETHING -> Refers to the to the total amount of tokens, i.e. this is the initialization for the length of list of tokens (with their own context?)
+                                        # Moet er een verbinding zijn tussen deze parameter en de input? -> I.e. moet ik dit uit de input halen of kan k gwn 3 invullen 
+                                        # Verwachting H1: Relevanter nummer is beter, maar kan door bruteforcen achterkomen'
+        
+        self.patch_embedding: int = 60  # Het aantal 'frames' in een sound segment; in relatie met lengte segmenten
+                                        # I.e, nu doen we dus 60 frames in elk segment van:
+        self.sound_length: int = 1      # 4 seconden    
 
     def encode(self, file_path) -> torch.tensor:
         """
@@ -21,9 +41,19 @@ class AudioTokenizer(GenericTokenizer):
         Audio is Quantized (whatever that means)
         Batched?
         """
-        waveform, sample_rate = torchaudio.load(file_path)
+        # Na het laden van de audio moet het dus sws eerst in een lijst per frames met x seconden gehakt worden
+        # Dit wordt dan opgehakt in kleine frames (*letter*lijk dit zijn tokens)
+        # uit de frames lezen we belangrijke data
+        # Dit is een tensor
+        # Daar wordt mee gerekend.
+
+        self.waveform, sample_rate = torchaudio.load(file_path) # Test 1: This is a very smallll tensor
         # Print out the waveform shape and sample rate for confirmation
-        return self.extract_features(waveform)
+        self.debug(f"min: {self.waveform.min()}")
+        self.debug(f" max: {self.waveform.max()}")
+        self.debug(f"mean: {self.waveform.mean()}")
+        self.debug(f"dtype: {self.waveform.dtype}")
+        return self.waveform
     
     def decode(self, tensor, path) -> str:
         """
@@ -83,3 +113,20 @@ class AudioTokenizer(GenericTokenizer):
 
     def pad_input(self, input: torch.Tensor, length: 32, dims: 3) -> torch.Tensor:
         return super().pad_input(input, length, dims)
+
+
+    def plot_spectrogram(specgram, title=None, ylabel="freq_bin", ax=None):
+        if ax is None:
+            _, ax = plt.subplots(1, 1)
+        if title is not None:
+            ax.set_title(title)
+        ax.set_ylabel(ylabel)
+        ax.imshow(librosa.power_to_db(specgram), origin="lower", aspect="auto", interpolation="nearest")
+
+
+    def plot_fbank(fbank, title=None):
+        fig, axs = plt.subplots(1, 1)
+        axs.set_title(title or "Filter bank")
+        axs.imshow(fbank, aspect="auto")
+        axs.set_ylabel("frequency bin")
+        axs.set_xlabel("mel bin")
